db1ffd29536a829be070b1d5bfb06a3473b8de80
Abhijeet <abhijeetjawle01@gmail.com>
2022-04-29 19:36:06 +0530

    Changed the heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Test App 2</h1>
+        <h2>This should be hosted on the subdomain app</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
