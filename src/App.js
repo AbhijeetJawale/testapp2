d8dc720ef18b3fdbecdaa7bf58c1aeaac8db037b
Abhijeet <abhijeetjawle01@gmail.com>
2022-04-29 20:03:47 +0530

    Changed layout v2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Test App 2</h1>
         <h2>This should be hosted on the subdomain app</h2>
         <p>
